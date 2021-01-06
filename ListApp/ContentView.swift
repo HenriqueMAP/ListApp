6cb9a9e5f8f0fc6418e33ebcbd877ff4d50bba1b
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-01-05 23:30:22 -0300

    List doesn’t working

--- ListApp/ContentView.swift
+++ ListApp/ContentView.swift
@@ -15,14 +15,13 @@ class DataSource: ObservableObject {
     
     init() {
         let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
         
-        if let path = Bundle.main.resourcePath, let items = try? fm.contentsOfDirectory(atPath: path) {
             for item in items {
-                if item.hasPrefix("nerd") {
-                    pictures.append(item)
-                }
+                if item.hasPrefix("nsl") {
+                    pictures.append(item) }
             }
-        }
         
         didChange.send(())
     }
@@ -34,6 +33,13 @@ struct DetailView: View {
     var body: some View {
         let img = UIImage(named: selectedImage)!
         Image(uiImage: img)
+            .resizable()
+            .aspectRatio(1024/768, contentMode: .fit)
+            .navigationBarTitle(Text(selectedImage), displayMode: .inline)
+            .navigationBarHidden(true)
+//            .onTapGesture {
+//                self.navigationBarHidden(false)
+//            }
     }
 }
 
@@ -41,14 +47,15 @@ struct ContentView: View {
     @ObservedObject var dataSource = DataSource()
     
     var body: some View {
-        NavigationView{
-            List(dataSource.pictures, id: \.self){ picture in
-                NavigationLink(LocalizedStringKey(picture), destination: DetailView(selectedImage: picture))
-                Text(picture)
+            NavigationView{
+                List(dataSource.pictures, id: \.self){ picture in
+                    Text(picture)
+                    NavigationLink(destination: DetailView(selectedImage: picture)){
+                        Text(picture)
+                        }
                 }.navigationBarTitle(Text("Storm Viewer"))
+            }
         }
-    }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
